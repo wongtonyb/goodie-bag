d71d9f2dcc8f2214a93678b43176ad975e7f5e9d
Tony Wong <wongtonyb@gmail.com>
2019-08-23 16:24:08 -0400

    backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
-'use strict'
+'use strict';
 
-const router = require('express').Router()
+const router = require('express').Router();
+const Candy = require('../db/models/Candy');
 
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
@@ -14,14 +15,25 @@ const router = require('express').Router()
 // And for your `/api/kittens` routes:
 // router.use('/kittens', require('./kittens'))
 
+router.get('/candies', async (req, res, next) => {
+  try {
+    let candies = await Candy.findAll();
+    if (candies) {
+      res.json(candies);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 // If someone makes a request that starts with `/api`,
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
 router.use((req, res, next) => {
-  const err = new Error('API route not found!')
-  err.status = 404
-  next(err)
-})
+  const err = new Error('API route not found!');
+  err.status = 404;
+  next(err);
+});
 
-module.exports = router
+module.exports = router;
--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -2,5 +2,31 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 module.exports = db.define('candy', {
-  // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      max: 10,
+    },
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: '',
+  },
 });
